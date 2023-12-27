efc3e64c208e97c72e95f6ac6f7c08aea043358b
youngeunkim <pkhj286@gmail.com>
2023-12-27 19:09:03 +0900

    렌더링

---
+++ src/pages/login/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import color from "../../styles/color";
+
+export const Layout = styled.div`
+  color: ${color.black};
+  width: 200px;
+  background-color: ${color.gray};
+`;
