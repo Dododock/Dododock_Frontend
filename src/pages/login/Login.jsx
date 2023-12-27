249fd77c7233dd459f99842dd5e09497cc2d3eeb
youngeunkim <pkhj286@gmail.com>
2023-12-27 18:02:44 +0900

    기본 세팅

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Modal from "react-modal";
+import * as S from "./Login";
 
 const Login = () => {
   return (
     <div>
-      return (<Modal isOpen={true}>모달입니다.</Modal>)
+      return (
+      <S.Modal>
+        <Modal isOpen={true}>모달입니다.</Modal>
+      </S.Modal>
+      )
     </div>
   );
 };
