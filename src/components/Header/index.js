627c0c63d0d9db83e4e8326943a013cff615c512
ìoxjadex <oharin765@gmail.com>
2023-12-27 19:57:33 +0900

    style: header 스타일

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import color from "../../styles/color";
 
 export const Layout = styled.div`
   position: sticky;
@@ -6,7 +7,7 @@ export const Layout = styled.div`
   align-items: center;
   gap: 50px;
   width: 100%;
-  height: 6vh;
+  height: 8vh;
   overflow: hidden;
   border-bottom: 1px solid #72837b;
 `;
@@ -17,12 +18,21 @@ export const Logo = styled.img`
 `;
 
 export const Emptybox = styled.div`
-  width: 75%;
+  width: 83%;
 `;
 
 export const Button = styled.button`
-  border-radius: 5px;
+  border-radius: 15px;
+  border: none;
+  box-shadow: 0px 1px 3px 0px;
+  background-color: ${color.white};
+  font-weight: 700;
+  font-family: pretendard;
 `;
+
 export const Login = styled.div`
-  margin: 5px;
+  margin-top: 3px;
+  margin-bottom: 3px;
+  margin-left: 7px;
+  margin-right: 7px;
 `;
