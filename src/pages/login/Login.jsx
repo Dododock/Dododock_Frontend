ae754aa2bf54fbfb0df08f4d309adb5bb7d8cce3
youngeunkim <pkhj286@gmail.com>
2023-12-27 17:43:51 +0900

    init

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "react-modal";
+
+const Login = () => {
+  return (
+    <div>
+      return (<Modal isOpen={true}>모달입니다.</Modal>)
+    </div>
+  );
+};
+
+export default Login;
