1283f1e44300d51024a67d998acb8abaf5d20519
ìoxjadex <oharin765@gmail.com>
2023-12-27 17:46:25 +0900

    feat: header

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  position: sticky;
+  display: flex;
+  align-items: center;
+  gap: 50px;
+  width: 100%;
+  height: 6vh;
+  overflow: hidden;
+  border-bottom: 1px solid #72837b;
+`;
+
+export const Logo = styled.img`
+  margin-left: 10px;
+  margin-top: 10px;
+  height: 60px;
+`;
+
+
+export const EmptyBox = styled.div`
+  width: 75%;
+`;
+
+export const Button = styled.button`
+  border-radius: 5px;
+`;
+
+export const Login = styled.div`
+  margin: 3px;
+`;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import Logo from "assets/dkdrla.png";
+import * as S from "./index.js";
+
+function Header() {
+  return (
+    <S.Layout>
+      <Link to="/">
+        <S.Logo src={Logo} alt="로고"></S.Logo>
+      </Link>
+      <S.EmptyBox />
+      <S.Button>
+        <S.Login>로그인</S.Login>
+      </S.Button>
+    </S.Layout>
+  );
+}
+
+export default Header;
