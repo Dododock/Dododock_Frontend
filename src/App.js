60bda571bf8981392d7efc59bf4242b1d17434e3
ìoxjadex <oharin765@gmail.com>
2023-12-28 08:25:29 +0900

    App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Detail from "./pages/detail/detail";
-import Group from "./pages/creatingGroup/index.jsx";
-import Card from "./pages/card/index.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/index.jsx";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/index.jsx";
+import Card from "./pages/card/index.jsx";
+import CreatingGroup from "./pages/creatingGroup/index.jsx";
 
 const App = () => {
   return (
     <>
-      <Home />
-      <Detail />
-      <Group />
-      <Card />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Card" element={<Card />} />
+          <Route path="/CreatingGroup" element={<CreatingGroup />} />
+        </Routes>
+      </Router>
       <Login />
     </>
   );
 };
-
 export default App;
