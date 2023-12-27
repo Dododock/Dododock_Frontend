9946d3484f3ca82a62c3c84b04354fc8c8dad8ef
ìoxjadex <oharin765@gmail.com>
2023-12-27 19:13:21 +0900

    충돌 해결

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import Login from "./pages/login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/index.jsx";
+import Login from "./pages/login/Login.jsx";
+import Home from "./pages/Home/index.jsx";
 
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import * as S from "./index.js";
+
+function Home() {
+  return <h1>홈입니다!</h1>;
+}
+
+export default Home;
