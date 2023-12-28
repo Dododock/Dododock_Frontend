804dff7d613e8251d3edeea45f76f3e2b2bd0dd8
ìoxjadex <oharin765@gmail.com>
2023-12-28 10:02:06 +0900

    feat: 그룹 생성 api

--- src/pages/creatingGroup/index.jsx
+++ src/pages/creatingGroup/index.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import * as S from "./index.js";
 import axiosInstance from "../util/axios";
 
-
 function Creating() {
+  const [groupName, setgroupName] = useState("");
+  const [simpleDescription, setsimpleDescription] = useState("");
+  const [memberCount, setmemberCount] = useState("");
+  const [schedule, setschedule] = useState("");
+  const [due, setdue] = useState("");
+
   const sendDataToDB = () => {
-    axiosInstance.post('/create', {
-      title: title,
-      detail: detail,
+    axiosInstance.post("/room/create", {
+      groupName: groupName,
+      simpleDescription: simpleDescription,
+      memberCount: memberCount,
+      schedule: schedule,
+      due: due,
     });
   };
   return (
@@ -16,26 +25,57 @@ function Creating() {
         <S.Box>
           <S.InputBox>
             <S.Text for="name">그룹 이름을 알려주세요.</S.Text>
-            <S.Input type="text" name="name"></S.Input>
+            <S.Input
+              type="text"
+              name="name"
+              placeholder="ex) 도도독"
+              onChange={(e) => setgroupName(e.target.value)}
+            ></S.Input>
           </S.InputBox>
           <S.InputBox>
             <S.Text for="name">어떤 그룹인지 알려주세요.</S.Text>
-            <S.Input type="text" name="type"></S.Input>
+            <S.Input
+              type="text"
+              name="type"
+              placeholder="최대 100자까지 가능합니다. "
+              onChange={(e) => setsimpleDescription(e.target.value)}
+            ></S.Input>
           </S.InputBox>
           <S.InputBox>
             <S.Text for="name">최대 참여 인원 수를 알려주세요.</S.Text>
-            <S.Input type="text" name="HeadCount"></S.Input>
+            <S.Input
+              type="number"
+              name="HeadCount"
+              placeholder="10명"
+              onChange={(e) => setmemberCount(e.target.value)}
+            ></S.Input>
           </S.InputBox>
           <S.InputBox>
             <S.Text for="name">1주일 활동 횟수를 알려주세요.</S.Text>
-            <S.Input type="number" name="WeekTime"></S.Input>
+            <S.Input
+              type="text"
+              name="WeekTime"
+              placeholder="ex) 1주일에 3번"
+              onClick={(e) => setschedule(e.target.value)}
+            ></S.Input>
           </S.InputBox>
           <S.InputBox>
             <S.Text for="name">그룹 모집 마감 날자를 알려주세요.</S.Text>
-            <S.Input type="date" name="DateInput"></S.Input>
+            <S.Input
+              type="date"
+              name="DateInput"
+              placeholder="ex) 1주일에 3번"
+              onClick={(e) => setdue(e.target.value)}
+            ></S.Input>
           </S.InputBox>
         </S.Box>
-        <S.Button>생성하기</S.Button>
+        <S.Button
+          onClick={() => {
+            sendDataToDB();
+          }}
+        >
+          생성하기
+        </S.Button>
       </S.InputContainer>
     </S.Layout>
   );
